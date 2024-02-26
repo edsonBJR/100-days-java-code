13571483931aa7c640feb76030a6ba34ee4e2585
BJREdson <bjr.edson@gmail.com>
2024-02-26 13:56:39 -0300

    Adjusting ignored files.

--- basics/basicsOfJava/src/main/java/org/example/Main.java
+++ basics/basicsOfJava/src/main/java/org/example/Main.java
@@ -2,6 +2,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        // Data Types
+        boolean booleanVar = true;
+        byte byteVar = 4;
+        short shortVar = 56;
+        int intVar = 123;
+        long longVar = 12121;
+        float floatVar = 1.12341324f;
+        double doubleVar = 4.12341234;
+        char charVar = 'G';
+
+        System.out.println(booleanVar);
+        System.out.println(byteVar);
+        System.out.println(shortVar);
+        System.out.println(intVar);
+        System.out.println(longVar);
+        System.out.println(floatVar);
+        System.out.println(doubleVar);
+        System.out.println(charVar);
     }
 }
