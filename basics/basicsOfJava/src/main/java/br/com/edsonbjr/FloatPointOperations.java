088579f99d4b2deaab4135cb71d3685fa38c992c
edsonBJR <ebjr.tux@gmail.com>
2024-02-27 10:18:18 -0300

    Work with float point with double data type.

---
+++ basics/basicsOfJava/src/main/java/br/com/edsonbjr/FloatPointOperations.java
@@ -0,0 +1,29 @@
+package br.com.edsonbjr;
+
+public class FloatPointOperations {
+
+    /*
+    Floating-Point Operations:
+    Write a Java program that declares two double variables,
+    assigns them values, and then performs addition, subtraction,
+    multiplication, and division operations on them. Print the results.
+     */
+
+    double a = 1.234;
+    double b = 2.987;
+
+    public FloatPointOperations() {}
+
+    public double addition(){
+        return a + b;
+    }
+    public double subtraction(){
+        return a - b;
+    }
+    public double multiplication(){
+        return a * b;
+    }
+    public double division(){
+        return a / b;
+    }
+}
---
+++ basics/basicsOfJava/src/test/java/br/com/edsonbjr/FloatPointOperationsTest.java
@@ -0,0 +1,38 @@
+package br.com.edsonbjr;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FloatPointOperationsTest {
+
+    private FloatPointOperations operation;
+    @Before
+    public void setUp() {
+        operation = new FloatPointOperations();
+    }
+
+    @Test
+    public void shouldReturnValueOfAddition() {
+        double result = operation.addition();
+        assertEquals(4.221, result, 0.001);
+    }
+
+    @Test
+    public void shouldReturnValueOfSubtraction() {
+        double result = operation.subtraction();
+        assertEquals(-1.753, result, 0.001);
+    }
+
+    @Test
+    public void shouldReturnValueOfMultiplication() {
+        double result = operation.multiplication();
+        assertEquals(3.685958, result, 0.001);
+    }
+
+    @Test
+    public void shouldReturnValueOfDivision() {
+        double result = operation.division();
+        assertEquals(0.413123535, result, 0.001);
+    }
+}
