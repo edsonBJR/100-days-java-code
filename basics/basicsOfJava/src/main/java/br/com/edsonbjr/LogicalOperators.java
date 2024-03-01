e22f1b8e8e2e4878ff76acc64e05e16fd89d7519
edsonBJR <ebjr.tux@gmail.com>
2024-03-01 16:51:24 -0300

    Comparing values at array and if this values are even

---
+++ basics/basicsOfJava/src/main/java/br/com/edsonbjr/LogicalOperators.java
@@ -0,0 +1,48 @@
+package br.com.edsonbjr;
+
+import java.util.Arrays;
+
+public class LogicalOperators {
+
+    /*
+        Logical Operators:
+            Declare two boolean variables and use logical
+            operators (&&, ||, !) to create compound conditions.
+            Print the results of the compound conditions.
+     */
+
+
+    // Method to compare arrays and store the results in a boolean array
+    public boolean[] compareArrays(int[] arr1, int[] arr2) {
+        // Check if arrays have the same length
+        if (arr1.length != arr2.length) {
+            throw new IllegalArgumentException("Arrays must have the same length.");
+        }
+
+        // Create a boolean array to store the results
+        int length = arr1.length;
+        boolean[] results = new boolean[length];
+
+        // Iterate through the arrays and compare values at corresponding indices
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                // Compare values at corresponding indices and check if both are even
+                if(arr1[i] == arr2[j] && isEven(arr1[i]) && isEven(arr2[j])) {
+                    results[i] = true;
+                } else {
+                    results[i] = false;
+                }
+            }
+        }
+
+        // Return the boolean array with the results
+        System.out.println(Arrays.asList(results));
+        return results;
+    }
+
+    // Method to check if a number is even
+    public boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+}
+
---
+++ basics/basicsOfJava/src/test/java/br/com/edsonbjr/LogicalOperatorsTest.java
@@ -0,0 +1,25 @@
+package br.com.edsonbjr;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class LogicalOperatorsTest {
+
+    @Test
+    public void testCompareArrays() {
+
+        LogicalOperators logicalOperators = new LogicalOperators();
+
+        int[] valuesA = {1, 2, 3, 4, 5};
+        int[] valuesB = {2, 6, 7, 8, 9};
+
+        boolean[] expectedResults = {false, true, false, false, false};
+        System.out.println(Arrays.asList(expectedResults[1]));
+        boolean[] actualResults = logicalOperators.compareArrays(valuesA, valuesB);
+        System.out.println(Arrays.asList(actualResults[1]));
+        assertArrayEquals(expectedResults, actualResults);
+    }
+}
