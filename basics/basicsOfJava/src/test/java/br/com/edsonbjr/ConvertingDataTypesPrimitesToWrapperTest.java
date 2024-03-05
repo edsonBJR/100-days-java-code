4c7fe2b5cb3224566da999857d994b1d269044a0
edsonBJR <ebjr.tux@gmail.com>
2024-03-05 10:47:41 -0300

    Finalizing units cover units tests.

--- basics/basicsOfJava/src/test/java/br/com/edsonbjr/ConvertingDataTypesPrimitesToWrapperTest.java
+++ basics/basicsOfJava/src/test/java/br/com/edsonbjr/ConvertingDataTypesPrimitesToWrapperTest.java
@@ -27,4 +27,39 @@ public class ConvertingDataTypesPrimitesToWrapperTest {
         assertEquals(Short.valueOf(value), result);
     }
 
+    @Test
+    public void shouldConvertlongToLong() {
+        long value = 10000L;
+        Long result = ConvertingDataTypesPrimitesToWrapper.convertlongToLong(value);
+        assertEquals(Long.valueOf(value), result);
+    }
+
+    @Test
+    public void shoulConvertfloatToFloat() {
+        float value = 1.1234F;
+        Float result = ConvertingDataTypesPrimitesToWrapper.convertfloatToFloat(value);
+        assertEquals(Float.valueOf(value), result);
+    }
+
+    @Test
+    public void shouldConvertdoubleToDouble() {
+        double value = 1.9876;
+        Double result = ConvertingDataTypesPrimitesToWrapper.convertdoubleToDouble(value);
+        assertEquals(Double.valueOf(value), result);
+    }
+
+    @Test
+    public void shouldConvertbooleanToBoolean() {
+        boolean value = false;
+        Boolean result = ConvertingDataTypesPrimitesToWrapper.convertbooleanToBoolean(value);
+        assertEquals(Boolean.valueOf(value), result);
+    }
+
+    @Test
+    public void shouldConvertCharToCharacter() {
+        char value = 42;
+        Character result = ConvertingDataTypesPrimitesToWrapper.convertCharToCharacter(value);
+        assertEquals(Character.valueOf(value), result);
+    }
+
 }
