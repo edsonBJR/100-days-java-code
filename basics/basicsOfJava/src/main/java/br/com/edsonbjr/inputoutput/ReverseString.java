65e640e57126414ad4ed64f8496beda45e567ecf
edsonBJR <ebjr.tux@gmail.com>
2024-03-08 13:50:58 -0300

    Reverse String using data type char.

---
+++ basics/basicsOfJava/src/main/java/br/com/edsonbjr/inputoutput/ReverseString.java
@@ -0,0 +1,21 @@
+package br.com.edsonbjr.inputoutput;
+
+public class ReverseString {
+
+    /*
+        Reverse String:
+            Read a string from the user and print its reverse.
+     */
+
+    public static void main(String[] args) {
+        String text = "Edson";
+        char[] charArray = text.toCharArray();
+        char[] newString = new char[charArray.length];
+
+        for (int i = charArray.length - 1, j = 0; i >= 0; --i, ++j) {
+            newString[j] = charArray[i];
+        }
+
+        System.out.println(newString);
+    }
+}
