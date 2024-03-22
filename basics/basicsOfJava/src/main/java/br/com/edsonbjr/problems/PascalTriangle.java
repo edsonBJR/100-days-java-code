c38f1a7550c6dd842f1d19fd0496728061d11edb
edsonBJR <ebjr.tux@gmail.com>
2024-03-22 15:46:43 -0300

    Pascals triangule solutions

---
+++ basics/basicsOfJava/src/main/java/br/com/edsonbjr/problems/PascalTriangle.java
@@ -0,0 +1,38 @@
+package br.com.edsonbjr.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+
+    /*
+        Given a non-negative integer numRows, generate the first numRows
+        of Pascal's Triangle
+     */
+
+    public List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> triangle = new ArrayList<>();
+        if (numRows == 0) return triangle;
+
+        List<Integer> first_row = new ArrayList<>();
+        first_row.add(1);
+        triangle.add(first_row);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prev_row = triangle.get(i - 1);
+            List<Integer> row = new ArrayList<>();
+
+            row.add(1);
+
+            for (int j = 1; j < i; j++) {
+                row.add(prev_row.get(j - 1) + prev_row.get(j));
+            }
+
+            row.add(1);
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+}
